99cc3c97bc3a27bd4f7c223182471744bc5bb10c
Pavel Mayorov <TA-19V-169@tptlive.ee>
2021-11-18 10:57:40 +0200

    Updated 181121 1057

--- base.ino
+++ base.ino
@@ -9,7 +9,7 @@ String GLOBAL_STATE = "";
 void setup()
 {
   // declaing some stuff
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
 }
 
@@ -31,16 +31,44 @@ bool task1(double speed, int distance_l, int distance_r, String start_from = "ba
     // user calls
 
     // asks speed
-
+    // NOTE: for now reading from serial
+    
+    // speed was not defined
+    if (GLOBAL_STATE == "task2")
+    {
+      if (speed <= 0)
+      {
+        speed = Serial.read();
+      }
+      start_from = Serial.read();
+    }
 
   }
   else 
   {
+    // save to local state
+    local_state = GLOBAL_STATE;
+
     // change global state
     GLOBAL_STATE = "task2";
 
     // func calls
-    local_state = GLOBAL_STATE;
   }
+
+  // check speed and start_from variables
+  if (speed > 0)
+  {
+    if (start_from != "baNaN") 
+    {
+      // user set starting point
+      
+    }
+  }
+  else 
+  {
+    Serial.print("ERROR");
+    return false;
+  }
+  
   
 }
